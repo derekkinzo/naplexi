1281dfac5cde633588b13dd26552f4a91e70cf21
Derek Kinzo <derek.kinzo@alumni.harvard.edu>
2022-05-03 00:28:35 -0600

    made production debug true

--- naplexi/production.py
+++ naplexi/production.py
@@ -5,7 +5,7 @@ import dj_database_url
 import django_heroku
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['naplexi.herokuapp.com',  'www.naplexi.com']
 
