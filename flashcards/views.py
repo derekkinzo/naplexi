ee308f8dbc8c68a7e4fcfef773fb5082b41b33ed
Derek Kinzo <derek.kinzo@alumni.harvard.edu>
2022-05-03 00:19:53 -0600

    Sample change to test settings

--- flashcards/views.py
+++ flashcards/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    print("Request for index page received")
+    print("Request for index page received!")
 
     context = {}
     return render(request, 'flashcards/index.html', context)
