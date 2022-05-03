9f9344e565a35abbcba9fa9222de769bc0a39faf
Derek Kinzo <derek.kinzo@alumni.harvard.edu>
2022-05-02 23:34:58 -0600

    updated project settings

---
+++ naplexi/settings/local.py
@@ -0,0 +1,17 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost']
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-dd1m(-id(c-b3*u2)y5l*a4dw&xvcx69tk!k-tlo4yvxa4y7tw'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
