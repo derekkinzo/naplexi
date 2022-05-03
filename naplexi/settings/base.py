30f65ff8b03711721269713ff2b8698b83148950
Derek Kinzo <derek.kinzo@alumni.harvard.edu>
2022-05-03 00:05:38 -0600

    Added static file storage

--- naplexi/settings/base.py
+++ naplexi/settings/base.py
@@ -117,6 +117,10 @@ STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static'),
 )
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
