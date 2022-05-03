fa2d742d63ebb5705b7198151dca84b6ed13df28
Derek Kinzo <derek.kinzo@alumni.harvard.edu>
2022-05-03 00:23:04 -0600

    Split settings files for production

--- naplexi/settings/base.py
+++ naplexi/settings/base.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dd1m(-id(c-b3*u2)y5l*a4dw&xvcx69tk!k-tlo4yvxa4y7tw'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['naplexi.herokuapp.com', 'naplexi.com', 'www.naplexi.com', '127.0.0.1']
 
 
 # Application definition
@@ -78,13 +69,7 @@ WSGI_APPLICATION = 'naplexi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -121,7 +106,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ naplexi/settings/production.py
@@ -0,0 +1,18 @@
+from .base import *
+
+import os
+import dj_database_url
+import django_heroku
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['naplexi.herokuapp.com',  'www.naplexi.com']
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+#Run this only on production (Heroku), otherwise travis tests fail with sqlite3 SCHEMA issue
+django_heroku.settings(locals())
